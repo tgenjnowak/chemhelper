45c51d0404ac9089caf4e52d25d0a8f9833636a9
Jan Stanstrup <stanstrup@gmail.com>
2014-03-14 14:44:56 +0100

    Added note about dependency on OpenBabel

--- R/chem_id_coverters.R
+++ R/chem_id_coverters.R
@@ -1,4 +1,5 @@
 smile2inchi =function(smile){
+  # calls obabel binary in system. openbabel need to be installed
   
   string=paste(as.character(smile),collapse='" -:"')
   output=system(      paste('obabel -ismi -:"',string,'" -oinchi --unique /nostereo',sep='')      ,intern=T,ignore.stderr = T)
