5079ea5a0dbd32740526c9d34b10b5bc49054a7f
Jan Stanstrup <stanstrup@gmail.com>
2014-03-17 15:25:13 +0100

    Added inchi2sdf and inchi.keep.cont and verbose output from obabel.

--- R/chem_id_coverters.R
+++ R/chem_id_coverters.R
@@ -1,8 +1,33 @@
-smile2inchi =function(smile){
+smile2inchi =function(smile,verbose=F){  
   # calls obabel binary in system. openbabel need to be installed
   
   string=paste(as.character(smile),collapse='" -:"')
-  output=system(      paste('obabel -ismi -:"',string,'" -oinchi --unique /nostereo',sep='')      ,intern=T,ignore.stderr = T)
+  output=system(      paste('obabel -ismi -:"',string,'" -oinchi',sep='')      ,intern=T,ignore.stderr = !verbose)
   
   return(output)
+}
+
+
+
+
+inchi2sdf =function(smile,verbose=F){
+  #require(ChemmineR)
+  # calls obabel binary in system. openbabel need to be installed
+  
+  string=paste(as.character(smile),collapse='" -:"')
+  output=system(      paste('obabel -iinchi -:"',string,'" -osdf --gen2D',sep='')      ,intern=T,ignore.stderr = !verbose)
+  
+  output=read.SDFset(output)
+
+  return(output)
+}
+
+
+inchi.keep.cont=function(inchi,verbose=F){
+  # calls obabel binary in system. openbabel need to be installed
+  
+  string=paste(as.character(inchi),collapse='" -:"')
+  output=system(      paste('obabel -iinchi -:"',string,'" -r -xr -oinchi',sep='')      ,intern=T,ignore.stderr = !verbose)
+  
+  return(output) 
 }
