5a68c953332740fdfeade4cfaa1783d129267116
Jan Stanstrup <stanstrup@gmail.com>
2014-03-18 10:36:58 +0100

    Added load.camera.rules

--- R/chem_id_coverters.R
+++ R/chem_id_coverters.R
@@ -1,3 +1,4 @@
+# smile2inchi ##################################
 smile2inchi =function(smile,verbose=F){  
   # calls obabel binary in system. openbabel need to be installed
   
@@ -10,6 +11,8 @@ smile2inchi =function(smile,verbose=F){
 
 
 
+
+# inchi2sdf ##################################
 inchi2sdf =function(inchi,verbose=F){
   #require(ChemmineR)
   # calls obabel binary in system. openbabel need to be installed
@@ -18,11 +21,16 @@ inchi2sdf =function(inchi,verbose=F){
   output=system(      paste('obabel -iinchi -:"',string,'" -osdf --gen2D',sep='')      ,intern=T,ignore.stderr = !verbose)
   
   output=read.SDFset(output)
-
+  
   return(output)
 }
 
 
+
+
+
+
+# inchi.keep.cont ##################################
 inchi.keep.cont=function(inchi,verbose=F){
   # calls obabel binary in system. openbabel need to be installed
   
@@ -31,3 +39,21 @@ inchi.keep.cont=function(inchi,verbose=F){
   
   return(output) 
 }
+
+
+
+
+
+# load.camera.rules ##################################
+load.camera.rules=function(mode){
+    
+  if (tolower(mode)=='pos') {ionmode=1}
+  if (tolower(mode)=='neg') {ionmode=2}
+  if (tolower(mode)=='ei')  {ionmode=3}
+  
+  
+  rules <- read.xlsx(system.file("extdata",paste('CAMERA_rules_',c('pos','neg','EI')[ionmode],'.xlsx',sep=""), package="chemhelper"),1)  
+  
+}
+
+
