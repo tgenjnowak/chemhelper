7c210ef9007f85dff2aa22a080b54d1a657b4711
Jan Stanstrup <stanstrup@gmail.com>
2014-03-24 16:26:00 +0100

    added example scripf for fixing orbitrap data

---
+++ inst/extdata/example_scripts/fix_orbitrap.R
@@ -0,0 +1,7 @@
+
+files=list.files('D:/FEM/Other_projects/Marynka/Positive mzXML solo FullScan/Batch 1/box 1',pattern='.mzXML',full.names=T)
+
+
+rem_satellite_peaks(files[1:2],outdir='D:/FEM/Other_projects/Marynka/fixedfiles')
+
+
