af1cc962db26a50684c6fb6d439c1f812c9a02f3
Jan Stanstrup <stanstrup@gmail.com>
2014-03-24 15:11:50 +0100

    started rem_satellite_peaks. Now only converts to mzML when needed. NO fixing yet

---
+++ R/file_convert.R
@@ -0,0 +1,41 @@
+getExt=function (path){
+  parts <- strsplit(path, '\\.')[[1]]
+  last <- parts[length(parts)]
+  last
+}
+
+
+
+
+
+
+
+rem_satellite_peaks=function(files,outdir){
+  
+
+  for (i in 1:length(files)){
+   
+    file=files[i]
+    
+    # openMS needs mzML files
+    if (!(getExt=='mzML')){
+      tempfile = tempfile(pattern = basename(sub("\\.[^.]*$", "", files[i]) ), tmpdir = outdir, fileext = ".mzXML")
+      
+      system(paste('msconvert --mzML "',files[i],'" -o "',outdir,'"',sep=""),intern=T)
+
+      
+    }
+    
+    
+    
+    
+  }  
+}
+
+
+
+
+
+C:\Program Files\OpenMS-1.11\bin>SpectraFilterWindowMower.exe -in "D:\temp\QC_Bo
+x6_AFTER_1.mzML" -out "D:\temp\test.mzML"  -algorithm:windowsize 0.3 -algorithm:
+  peakcount 1
