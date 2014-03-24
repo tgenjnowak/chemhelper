72fdba4468e7f7d91a03626d3c84aa512bd8371e
Jan Stanstrup <stanstrup@gmail.com>
2014-03-24 15:35:50 +0100

    rem_satellite_peaks done

--- R/file_convert.R
+++ R/file_convert.R
@@ -17,25 +17,23 @@ rem_satellite_peaks=function(files,outdir){
    
     file=files[i]
     
-    # openMS needs mzML files
+    # openMS needs mzML files so we use msconvert to convert first of we don't have mzML
     if (!(getExt(files[i])=='mzML')){
       tempfile = tempfile(pattern = basename(sub("\\.[^.]*$", "", files[i]) ), tmpdir = outdir, fileext = ".mzXML")
-      
       system(paste('msconvert --mzML "',files[i],'" -o "',outdir,'"',sep=""),intern=T)
-
-      
+      file=tempfile
     }
     
     
+        # do the fixing    
+    system(paste('SpectraFilterWindowMower -in "',file,'" -out "',outdir,'/',basename(sub("\\.[^.]*$", "", files[i])),'.mzML','" -algorithm:windowsize 0.3 -algorithm:peakcount 1',sep=""),intern=T)
     
     
+    # delete temp mzML
+    if (!(getExt(files[i])=='mzML')){
+      unlink(tempfile)
+    }
   }  
 }
 
 
-
-
-
-#C:\Program Files\OpenMS-1.11\bin>SpectraFilterWindowMower.exe -in "D:\temp\QC_Bo
-#x6_AFTER_1.mzML" -out "D:\temp\test.mzML"  -algorithm:windowsize 0.3 -algorithm:
-  #peakcount 1
