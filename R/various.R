542e140a944aec9a2e06696cf01b78517513d925
Jan Stanstrup <stanstrup@gmail.com>
2014-03-28 12:01:34 +0100

    Added function to copy paste to clipboard on linux. needs xclip

---
+++ R/various.R
@@ -0,0 +1,5 @@
+clipboard <- function(x, sep="\t", row.names=FALSE, col.names=TRUE){
+  con <- pipe("xclip -selection clipboard -i", open="w")
+  write.table(x, con, sep=sep, row.names=row.names, col.names=col.names)
+  close(con)
+}
