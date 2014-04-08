5916a9a4a814fb0d5e7741f1a32863f8e9724d26
Jan Stanstrup <stanstrup@gmail.com>
2014-04-08 15:24:32 +0200

    Colored samples according to group. Ungrouped samples appear as an open square.

--- R/xcms_helpers.R
+++ R/xcms_helpers.R
@@ -16,15 +16,15 @@ analyze.xcms.group=function(xset_grouped,mz,rt,rt_tol_sample=60,mz_tol_sample=0.
   
   # grouped data
   select_g = abs(xset_grouped@groups[,"rtmed"]-rt)<rt_tol_group & abs(xset_grouped@groups[,"mzmed"]-mz)<mz_tol_group
-  sum(select_g)
-  
+  select_g = which(select_g)
   group_report=cbind(rtmed=xset_grouped@groups[select_g,"rtmed"]/60,rtmin=xset_grouped@groups[select_g,"rtmin"]/60,rtmax=xset_grouped@groups[select_g,"rtmax"]/60,mzmed=xset_grouped@groups[select_g,"mzmed"],mzmin=xset_grouped@groups[select_g,"mzmin"],mzmax=xset_grouped@groups[select_g,"mzmax"])
   
   
   #plot
-  plot(xset_grouped@peaks[select,"rt"]/60,xset_grouped@peaks[select,"mz"],pch=20,             ylim=c(min(xset_grouped@groups[select_g,"mzmin"]),max(xset_grouped@groups[select_g,"mzmax"])),                     ,xlim=c(min(xset_grouped@groups[select_g,"rtmin"])/60,max(xset_grouped@groups[select_g,"rtmax"])/60)    )
+  plot(xset_grouped@peaks[select,"rt"]/60,xset_grouped@peaks[select,"mz"],pch=0,cex=0.5,             ylim=c(min(xset_grouped@groups[select_g,"mzmin"]),max(xset_grouped@groups[select_g,"mzmax"])),                     ,xlim=c(min(xset_grouped@groups[select_g,"rtmin"])/60,max(xset_grouped@groups[select_g,"rtmax"])/60)    ,xlab="rt (min)",ylab="m/z", main=paste("Grouping of m/z=",mz,", rt=",rt/60,"min",sep=""))
     
   for (i in 1:nrow(group_report)){
+    points(    xset_grouped@peaks[  xset_grouped@groupidx[[select_g[i]]]   ,"rt"] /60 ,  xset_grouped@peaks[  xset_grouped@groupidx[[select_g[i]]]   ,"mz"]    ,col=i,pch=20)
     rect(group_report[i,"rtmin"], group_report[i,"mzmin"], group_report[i,"rtmax"], group_report[i,"mzmax"],border=NA,col=alpha(i, 0.5))
   }
   
