536c2aae7627979ecb84ce99f774d54ab52008b6
Jan Stanstrup <stanstrup@gmail.com>
2014-04-08 14:31:11 +0200

    Added analyze.xcms.group

---
+++ R/xcms_helpers.R
@@ -0,0 +1,36 @@
+analyze.xcms.group=function(xset_grouped,mz,rt,rt_tol_sample=60,mz_tol_sample=0.01,rt_tol_group=30,mz_tol_group=0.05){
+  
+  ## Ungrouped data. In which samples is it found?
+  select = abs(xset_grouped@peaks[,"rt"]-rt)<rt_tol_sample & abs(xset_grouped@peaks[,"mz"]-mz)<mz_tol_sample
+  if(sum(select)==0){stop("No peaks found.")}
+  
+  
+  # xset_grouped@filepaths[       unique(xset_grouped@peaks[select,"sample"])       ]
+  # sum(select)
+  # 
+  # min(xset_grouped@peaks[select,"rt"])/60
+  # max(xset_grouped@peaks[select,"rt"])/60
+  # min(xset_grouped@peaks[select,"mz"])
+  # max(xset_grouped@peaks[select,"mz"])
+  
+  
+  # grouped data
+  select_g = abs(xset_grouped@groups[,"rtmed"]-rt)<rt_tol_group & abs(xset_grouped@groups[,"mzmed"]-mz)<mz_tol_group
+  sum(select_g)
+  
+  group_report=cbind(rtmed=xset_grouped@groups[select_g,"rtmed"]/60,rtmin=xset_grouped@groups[select_g,"rtmin"]/60,rtmax=xset_grouped@groups[select_g,"rtmax"]/60,mzmed=xset_grouped@groups[select_g,"mzmed"],mzmin=xset_grouped@groups[select_g,"mzmin"],mzmax=xset_grouped@groups[select_g,"mzmax"])
+  
+  
+  #plot
+  plot(xset_grouped@peaks[select,"rt"]/60,xset_grouped@peaks[select,"mz"],pch=20,             ylim=c(min(xset_grouped@groups[select_g,"mzmin"]),max(xset_grouped@groups[select_g,"mzmax"])),                     ,xlim=c(min(xset_grouped@groups[select_g,"rtmin"])/60,max(xset_grouped@groups[select_g,"rtmax"])/60)    )
+  
+  points(rt/60,mz,col="red",pch=20,cex=3)
+  
+  for (i in 1:nrow(group_report)){
+    rect(group_report[i,"rtmin"], group_report[i,"mzmin"], group_report[i,"rtmax"], group_report[i,"mzmax"],border=NA,col=alpha(i, 0.5))
+  }
+  
+  
+  return(group_report)
+  
+}
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 chemhelper
 ==========
 
-R functions helpful in working with chemical data.
+R functions helpful in working with chemical data. A number of functions help analyze metabolomics data processed with xcms/CAMERA.
 * Converts files from Waters .raw format to mzData (convert.waters(indir,outdir)). MassLynx need to be installed and masswolf need to be in path.
 * Extended list of adducts/fragments for CAMERA (load.camera.rules)
 * Annotated mz/rt from dataset by database of known compounds' mz/rt (db.comp.assign(mz,rt,comp_name_db,mz_db,rt_db,mzabs=0.01,ppm=15,ret_tol=30))
+* For XCMS data: Why is my feature not listed? Try analyze.xcms.group too see if it picked in the files and if it was added to a group.
 * pubchem2inchi
 * inchi2sdf
 * smile2inchi
