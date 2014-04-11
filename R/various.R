e22b85f026aa9e373564ba4e536e7024f419d3fb
Jan Stanstrup <stanstrup@gmail.com>
2014-04-11 15:59:15 +0200

    Added google_ss to read google docs spreadsheets (old version only)

--- R/various.R
+++ R/various.R
@@ -19,3 +19,17 @@ chemhelper.update=function(){
   install_github(repo = "chemhelper", username = "stanstrup")
   library(chemhelper)
 }
+
+
+
+
+google_ss <- function(gid = NA, key = NA) 
+{
+  if (is.na(gid)) {stop("\nWorksheetnumber (gid) is missing\n")}
+  if (is.na(key)) {stop("\nDocumentkey (key) is missing\n")}
+  
+  url <- getURL(paste("https://docs.google.com/spreadsheet/pub?key=", key,
+                      "&single=true&gid=", gid, "&output=csv", sep = ""),
+                cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+  read.csv(textConnection(url), header = T, sep = ",")
+}
