88fb85a606511f81be193dd91d219965c0ea601a
Jan Stanstrup <stanstrup@gmail.com>
2014-05-20 11:21:47 +0200

    mass_decompose: changed "mode" to "ion" and added many ions as options. Also allowed to decompose several ions in one go. Output is now a list unless simplify is specified.

--- R/calculations.R
+++ R/calculations.R
@@ -1,42 +1,66 @@
-mass_decompose = function(mass,mode="neutral",elements=c('C','H','N','O','P','S'),ppm=30, filter=TRUE) 
+mass_decompose = function(mass,ion="neutral",elements=c('C','H','N','O','P','S'),ppm=30, filter=TRUE,simplify=TRUE) 
 {
   
-  
-  
-  # Get the neutral mass
-  if(tolower(mode)=="pos"){mode=1}
-  if(tolower(mode)=="neg"){mode=2}
-  if(tolower(mode)=="neutral"){mode=3}
-  
-  mass=mass+1.0073*c(-1,1,0)[mode]
-  
-  
-  # Shortcut to allow more masses
-  if(tolower(elements)[1]=="expand"){
-    elements=c('C','H','N','O','P','S','Na','K','Cl','Br','Fe','Al','Cu','Zn','As')
-  }
-  
-  
-  # Do the decomposing
   elements=initializeElements(elements)
-  formulas=decomposeMass(mass,ppm=30,elements=elements)
+  formulas_list=list()
   
-  # Make nice table of results
-  formulas=cbind(getFormula(formulas),getValid(formulas),formulas$DBE,signif(getMass(formulas)+1.0073*c(1,-1,0)[mode],digits=7), signif((getMass(formulas)-(mass))/(mass)*1e6,digits=3), signif(getScore(formulas),digits=3))
-  
-  colnames(formulas)=c("Formula","Valid","DBE","Calc. m/z","ppm","Score")
-  
-  
-  if(filter){
-    formulas=formulas[   floor(as.numeric(formulas[,"DBE"])) == as.numeric(formulas[,"DBE"]) & as.numeric(formulas[,"DBE"])>-1 & formulas[,"Valid"]=="Valid"       ,,drop=F]
+  for(i in 1:length(ion)){
+    
+    
+    offset <- 
+      switch(tolower(ion[i]),
+             "neutral" = 0,
+             "pos" = 1.007276455,
+             "neg" = -1.007276455,
+             "[m+h]+" = 1.007276455,
+             "[m-h]-" = -1.007276455,
+             
+             "[m+na]+" = 22.989218,
+             "[m+k]+" = 38.963158,
+             
+             "[m-2h+na]-" = 20.974666,
+             "[m-2h+k]-" = 36.948606,
+             "[m+cl]-" = 34.969402,
+             "[m-h+hcoona]-" = 66.980164,
+             "[m-h+hcooh]-" = 44.998194
+      )
+    
+    
+    if(is.null(offset)){stop("The selected ion is not supported")}
+    
+    # Get the neutral mass
+    mass_offsetted=mass-offset
+    
+    
+    # Shortcut to allow more masses
+    if(tolower(elements)[1]=="expand"){
+      elements=c('C','H','N','O','P','S','Na','K','Cl','Br','Fe','Al','Cu','Zn','As')
+    }
+    
+    
+    # Do the decomposing
+    formulas=decomposeMass(mass_offsetted,ppm=30,elements=elements)
+    
+    # Make nice table of results
+    formulas=cbind(getFormula(formulas),getValid(formulas),formulas$DBE,signif(getMass(formulas)+offset,digits=7), signif((getMass(formulas)-(mass_offsetted))/(mass_offsetted)*1e6,digits=3), signif(getScore(formulas),digits=3))
+    
+    colnames(formulas)=c("Formula","Nitrogen rule","DBE","Calc. m/z","ppm","Rdisop score")
+    
+    
+    if(filter){
+      formulas=formulas[   floor(as.numeric(formulas[,"DBE"])) == as.numeric(formulas[,"DBE"]) & as.numeric(formulas[,"DBE"])>-1 & formulas[,"Nitrogen rule"]=="Valid"       ,,drop=F]
+    }
+    
+    
+    
+    formulas_list[[i]]=formulas
   }
   
   
+  if(simplify){
+    if(length(formulas_list)==1){formulas_list=formulas_list[[1]]}
+  }
   
-  return(formulas)
-  
-  
-  
-  
+  return(formulas_list)
   
 }
