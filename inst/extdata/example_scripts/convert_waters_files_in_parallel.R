70a97cd2a058d56f8bdc5d509fd27471367e601d
Jan Stanstrup <stanstrup@gmail.com>
2014-06-27 11:12:27 +0200

    better way to detect number of cores

--- inst/extdata/example_scripts/convert_waters_files_in_parallel.R
+++ inst/extdata/example_scripts/convert_waters_files_in_parallel.R
@@ -38,7 +38,7 @@ arg_list[[i]]=c(indir[i],outdir[i])
 
 
 # Do parallel conversion
-cl <- makeCluster(getOption("cl.cores", 2))
+cl <- makeCluster(detectCores())
 clusterExport(cl, "convert.waters")
 parLapplyLB(cl,arg_list,function(x) convert.waters(x[1],x[2]))
 stopCluster(cl)
