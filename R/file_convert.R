12b4c6ea2523b78ab173cb398715f9ef6b9bd4ab
Jan Stanstrup <stanstrup@gmail.com>
2014-06-27 10:56:09 +0200

    function (merge_scan_events) to merge odd and even scans. This should be used in case the mass range was split between two scan events.

--- R/file_convert.R
+++ R/file_convert.R
@@ -580,3 +580,104 @@ close(fileConn)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+merge_scan_events=function(infile,outfile){
+
+
+
+xraw = xcmsRaw(infile,profstep = 0)
+
+# Get all scans seperately
+scans=list()
+scanrange = matrix(nrow = length(xraw@scanindex),ncol=2)
+colnames(scanrange)=c("mzmin","mzmax")
+for (i in 1:length(xraw@scanindex)){
+  scans[[i]] = getScan(xraw,i)
+  scanrange[i,"mzmin"]=min(scans[[i]][,"mz"])
+  scanrange[i,"mzmax"]=max(scans[[i]][,"mz"])
+}
+
+
+# Check if larger mass scan are always after lower mass scan
+scan_order_check = matrix(nrow = 0,ncol=2)
+colnames(scanrange)=c("mzmin","mzmax")
+
+for( i in seq(from=1, to=nrow(scanrange)-1,by=2)){
+  temp =   scanrange[i,"mzmin"]<scanrange[i+1,"mzmin"]
+  temp[2] =   scanrange[i,"mzmax"]<scanrange[i+1,"mzmax"]
+  names(temp)=c("mzmin","mzmax")
+  scan_order_check = rbind(scan_order_check,temp)
+}
+
+# If not then stop
+if(!(all(scan_order_check)))
+  stop("Scans are not in order. No implementation to handle this yet.")
+
+
+
+
+#Merge the scans
+scans_new=list()
+odd_scans = seq(from=1, to=nrow(scanrange)-1,by=2)
+for( i in 1:length(odd_scans)){
+  scans_new[[i]]=rbind(  scans[[odd_scans[i]]],  scans[[odd_scans[i]+1]]             )
+}
+
+
+#make sure mz are ordered
+for( i in 1:length(scans_new)){
+  order = order(scans_new[[i]][,"mz"])
+  scans_new[[i]]=scans_new[[i]][order,]
+}
+
+
+# Create new scanindex
+scanindex_new=numeric()
+for (i in 1:length(scans_new)){
+  if (i==1){
+    scanindex_new[i]=0
+  }else{
+    scanindex_new[i] = scanindex_new[i-1] + nrow(scans_new[[(i-1)]])
+  }
+}
+
+
+
+
+
+
+# Put data into new object
+ob<-new("xcmsRaw")
+ob@env = new.env(parent=.GlobalEnv)
+
+scans_new=do.call(rbind,scans_new)
+ob@env$mz<-scans_new[,'mz']
+ob@env$intensity<-scans_new[,'intensity']
+
+ob@scantime <-xraw@scantime[odd_scans]
+ob@scanindex <-as.integer(scanindex_new)
+ob@polarity   <- xraw@polarity[odd_scans]
+ob@acquisitionNum <- 1:length(ob@scanindex)
+ob@profmethod     <- xraw@profmethod
+ob@mzrange        <- xraw@mzrange
+ob@filepath       <- xraw@filepath
+profStep(ob) =   profStep(xraw)
+ob=remakeTIC(ob)
+
+
+dir.create(dirname(outfile),showWarnings=F)
+write.mzdata(ob,outfile)
+
+
+}
---
+++ inst/extdata/example_scripts/merge_scan_events_in_parallel.R
@@ -0,0 +1,23 @@
+require(chemhelper)
+require(parallel)
+require(stringr)
+
+infiles = list.files(path="/media/jan/storage/FEM/Other_projects/pancreatic cancer/org_data",pattern=".mzXML",recursive = T,full.names = T)
+outfiles = str_replace(infiles,"org_data","scans_merged")
+
+
+
+
+# combine infiles and outfiles in a list so it can be passed to apply functions
+arg_list=list()
+for (i in 1:length(infiles)){
+  arg_list[[i]]=c(infiles[i],outfiles[i])
+}
+
+
+
+# Do parallel conversion
+cl <- makeCluster(getOption("cl.cores", 2))
+clusterExport(cl, "merge_scan_events")
+parLapplyLB(cl,arg_list,function(x) merge_scan_events(x[1],x[2]))
+stopCluster(cl)
