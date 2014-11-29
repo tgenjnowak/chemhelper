82fcc492ef782967faca52f36484feabcaa7f776
Jan Stanstrup <stanstrup@gmail.com>
2014-11-29 23:20:15 +0100

    added xsAnnotate_stats.

--- R/xcms_helpers.R
+++ R/xcms_helpers.R
@@ -33,3 +33,103 @@ analyze.xcms.group=function(xset_grouped,mz,rt,rt_tol_sample=60,mz_tol_sample=0.
   return(group_report)
   
 }
+
+
+
+
+
+
+
+xsAnnotate_stats <- function(object){
+
+# get the peaklist
+peaklist=getPeaklist(object)
+
+
+# setup output
+annotation_statistics=matrix(ncol=2,nrow=10)
+
+annotation_statistics[1,1]='# Feature'
+annotation_statistics[2,1]='# Feature groups'
+annotation_statistics[3,1]='# Features groups with several features'
+annotation_statistics[4,1]='# Features in feature groups with several features'
+
+annotation_statistics[5,1]='# Features annotated as isotopes'
+annotation_statistics[6,1]='# Feature with adducts/fragments annotation'
+annotation_statistics[7,1]='# Feature groups with fragment/adduct annotation'
+annotation_statistics[8,1]='# Annotation groups'
+annotation_statistics[9,1]='Mean number of annotation groups per feature group with annotations'
+annotation_statistics[10,1]='Median number of annotation groups per feature group with annotations'
+
+
+
+# Feature
+annotation_statistics[1,2]=nrow(peaklist['adduct'])
+
+
+# Feature groups
+annotation_statistics[2,2]=length(unique(peaklist[,'pcgroup']))
+
+
+# Features groups with several features
+annotation_statistics[3,2]=
+  paste(
+    sum(table(peaklist[,'pcgroup'])>1),
+    ' (',
+    round(  sum(table(peaklist[,'pcgroup'])>1)           /       length(unique(peaklist[,'pcgroup'])) *100   ,0)    ,
+    ' %)', sep='')
+
+
+# Features in feature groups with several features
+annotation_statistics[4,2]=
+  paste(
+    sum(table(peaklist[,'pcgroup'])    [table(peaklist[,'pcgroup'])>1]),
+    ' (',
+    round(  sum(table(peaklist[,'pcgroup'])    [table(peaklist[,'pcgroup'])>1])         /          nrow(peaklist['adduct'])*100      ,)    ,
+    ' %)', sep='')
+
+
+
+# Features annotated as isotopes
+annotation_statistics[5,2]=
+  paste(
+    nrow(object@isoID),
+    ' (',
+    round(  nrow(object@isoID)   /  sum(table(peaklist[,'pcgroup'])    [table(peaklist[,'pcgroup'])>1])*100      ,)    ,
+    ' %)', sep='')
+
+
+
+# Feature with adducts/fragments annotation
+annotation_statistics[6,2]=
+  paste(
+    sum(peaklist['adduct']!=''),
+    ' (',
+    round(  sum(peaklist['adduct']!='') / (nrow(peaklist['adduct']) - nrow(object@isoID))*100      ,)    ,
+    ' %)', sep='')
+
+
+
+# Feature groups with fragment/adduct annotation
+annotation_statistics[7,2]=
+  paste(
+    length(table(object@annoGrp[,'psgrp'])),
+    ' (',
+    round(  length(table(object@annoGrp[,'psgrp']))  / sum(table(peaklist[,'pcgroup'])>1)*100      ,)    ,
+    ' %)', sep='')
+
+
+
+# Annotation groups
+annotation_statistics[8,2]=nrow((object@annoGrp))
+
+#Mean number of annotation groups per feature group with annotations
+annotation_statistics[9,2]=round(mean(table(object@annoGrp[,'psgrp']))  ,1)
+
+#Median number of annotation groups per feature group with annotations
+annotation_statistics[10,2]=median(table(object@annoGrp[,'psgrp']))
+
+
+return(annotation_statistics)
+
+}
