0e5fce4dd740349da3b9617917c9638460f1e1a2
Jan Stanstrup <stanstrup@gmail.com>
2015-01-15 14:15:24 +0100

    group_correct only worked correctly if groups were sorted.

--- R/data_transformations.R
+++ R/data_transformations.R
@@ -1,14 +1,19 @@
-group_correct=function (dataset,f,zero.is.na=F){
+group_correct <- function (dataset,f,zero.is.na=F){
   
-  dataset_fixed=dataset
+  # copy dataset
+  dataset_fixed <- dataset
   
+   # Replace na with zero
   if (zero.is.na) { 
     is.zero = dataset_fixed==0
     dataset_fixed[is.zero]=NA
   }
   
   
-  new_matrix = apply(dataset_fixed,2,function(x) {
+  split_order_reverse <- order(unlist(split(1:length(f),f)))
+  
+  
+  new_matrix <- apply(dataset_fixed,2,function(x) {
     
     overall_mean=mean(x,na.rm=T)
     split_data = split(x,f)
@@ -17,15 +22,17 @@ group_correct=function (dataset,f,zero.is.na=F){
     
   })
   
-  colnames(new_matrix) = colnames(dataset)
-  rownames(new_matrix) = rownames(dataset)
+  new_matrix <- new_matrix[split_order_reverse,]
+  
+  colnames(new_matrix) <- colnames(dataset)
+  rownames(new_matrix) <- rownames(dataset)
 
   if (zero.is.na) { 
     new_matrix[is.zero]=0
   }
   
   
-  if(is.data.frame(dataset)) {new_matrix=as.data.frame(new_matrix)}
+  if(is.data.frame(dataset)) {new_matrix <- as.data.frame(new_matrix)}
   
   return(new_matrix)
   
