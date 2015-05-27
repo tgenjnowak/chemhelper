fbabc9061b1b3f0eaa63a7983b6380f04837a133
Jan Stanstrup <stanstrup@gmail.com>
2015-05-27 14:07:58 +0200

    Updated to give the rt standard deviation of each group. Helpful in setting the bw parameter in the group function.

--- R/xcms_helpers.R
+++ R/xcms_helpers.R
@@ -17,18 +17,30 @@ analyze.xcms.group=function(xset_grouped,mz,rt,rt_tol_sample=60,mz_tol_sample=0.
   # grouped data
   select_g = abs(xset_grouped@groups[,"rtmed"]-rt)<rt_tol_group & abs(xset_grouped@groups[,"mzmed"]-mz)<mz_tol_group
   select_g = which(select_g)
-  group_report=cbind(rtmed=xset_grouped@groups[select_g,"rtmed"]/60,rtmin=xset_grouped@groups[select_g,"rtmin"]/60,rtmax=xset_grouped@groups[select_g,"rtmax"]/60,mzmed=xset_grouped@groups[select_g,"mzmed"],mzmin=xset_grouped@groups[select_g,"mzmin"],mzmax=xset_grouped@groups[select_g,"mzmax"])
+  group_report <- cbind(`rtmed (min)` = xset_grouped@groups[select_g,"rtmed"]/60,
+                        `rtmin (min)` = xset_grouped@groups[select_g,"rtmin"]/60,
+                        `rtmax (min)` = xset_grouped@groups[select_g,"rtmax"]/60,
+                        mzmed         = xset_grouped@groups[select_g,"mzmed"],
+                        mzmin         = xset_grouped@groups[select_g,"mzmin"],
+                        mzmax         = xset_grouped@groups[select_g,"mzmax"])
   
   
   #plot
   plot(xset_grouped@peaks[select,"rt"]/60,xset_grouped@peaks[select,"mz"],pch=0,cex=0.5,             ylim=c(min(xset_grouped@groups[select_g,"mzmin"]),max(xset_grouped@groups[select_g,"mzmax"])),                     ,xlim=c(min(xset_grouped@groups[select_g,"rtmin"])/60,max(xset_grouped@groups[select_g,"rtmax"])/60)    ,xlab="rt (min)",ylab="m/z", main=paste("Grouping of m/z=",mz,", rt=",rt/60,"min",sep=""))
     
+  rt_sd <- c()
   for (i in 1:nrow(group_report)){
-    points(    xset_grouped@peaks[  xset_grouped@groupidx[[select_g[i]]]   ,"rt"] /60 ,  xset_grouped@peaks[  xset_grouped@groupidx[[select_g[i]]]   ,"mz"]    ,col=i,pch=20)
-    rect(group_report[i,"rtmin"], group_report[i,"mzmin"], group_report[i,"rtmax"], group_report[i,"mzmax"],border=NA,col=alpha(i, 0.5))
+    rt_points <- xset_grouped@peaks[  xset_grouped@groupidx[[select_g[i]]]   ,"rt"] /60
+    rt_sd[i]  <- sd(rt_points*60)
+    
+    points(   rt_points  ,  xset_grouped@peaks[  xset_grouped@groupidx[[select_g[i]]]   ,"mz"]    ,col=i,pch=20)
+    rect(group_report[i,"rtmin (min)"], group_report[i,"mzmin"], group_report[i,"rtmax (min)"], group_report[i,"mzmax"],border=NA,col=alpha(i, 0.5))
   }
   
   points(rt/60,mz,col="red",pch=20,cex=3)
+  
+  
+  group_report <- cbind.data.frame(group_report,`rt_sd (s)` = rt_sd)
   
   return(group_report)
   
