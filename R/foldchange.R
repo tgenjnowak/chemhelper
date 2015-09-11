0c6017aef50a5211113f816b04af2d243dfbf59e
Jan Stanstrup <stanstrup@gmail.com>
2015-09-12 00:29:06 +0200

    Added two new methods for fold change calculation

--- R/foldchange.R
+++ R/foldchange.R
@@ -20,14 +20,16 @@ ratio.w.invert <- function(x,y){
 
 
 
-fold.change <- function(MAT,f,aggr_FUN=mean,combi_FUN = function(x,y) "/"(x,y)    ,method=1){
+fold.change <- function(MAT,f,aggr_FUN=mean,combi_FUN = function(x,y) "/"(x,y)    ,method=5){
   
-if(method==1){  out <-  fold.change1(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
-if(method==2){  out <-  fold.change2(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
-if(method==3){  out <-  fold.change3(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
-if(method==4){  out <-  fold.change3(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
-
-return(out)
+  if(method==1){  out <-  fold.change1(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
+  if(method==2){  out <-  fold.change2(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
+  if(method==3){  out <-  fold.change3(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
+  if(method==4){  out <-  fold.change4(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
+  if(method==5){  out <-  fold.change5(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
+  if(method==6){  out <-  fold.change6(MAT=MAT,f=f,aggr_FUN=aggr_FUN,combi_FUN=combi_FUN)       }
+  
+  return(out)
 }
 
 
@@ -40,7 +42,7 @@ return(out)
 
 
 fold.change1 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   ){
-
+  
   f_un <- unique(f)
   temp <- matrix(NA,nrow = length(f_un),ncol=ncol(MAT))
   rownames(temp) <- f_un
@@ -62,6 +64,7 @@ fold.change1 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   
   }
   
   
+  colnames(out) <- colnames(MAT)
   return(out)
 }
 
@@ -86,6 +89,7 @@ fold.change2 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   
   })
   
   
+  colnames(out) <- colnames(MAT)
   return(out)
 }
 
@@ -93,7 +97,7 @@ fold.change2 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   
 
 
 fold.change3 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)     ){
-    
+  
   temp <- recast(data.frame(class=f,MAT),class ~ variable,id.var="class",aggr_FUN)
   
   
@@ -113,6 +117,7 @@ fold.change3 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   
   }
   
   
+  colnames(out) <- colnames(MAT)
   return(out)
 }
 
@@ -125,9 +130,9 @@ fold.change3 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   
 
 
 fold.change4 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)     ){
-
+  
   # mean by purrr package
-    temp <- data.frame(class = f, MAT) %>%
+  temp <- data.frame(class = f, MAT) %>%
     slice_rows("class") %>%
     by_slice(map, aggr_FUN)
   
@@ -147,7 +152,53 @@ fold.change4 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   
     out[i,] <- combi_FUN_vec(    temp[combs[i,1],]      ,         temp[combs[i,2],]   )
   }
   
+  colnames(out) <- colnames(MAT)
   return(out)
 }
 
+
+
+
+
+
+fold.change5 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   ){
+  
+  # mean using purrr
+  x <- data.frame(class = f, MAT) %>%  slice_rows("class") %>% by_slice(map, aggr_FUN)
+  rownames <- as.character(as.data.frame(x[,1])[,1])
+  x <- as.matrix(x[,-1])
+  rownames(x) <- rownames
+  
+  # calculate changes between all rows
+  i <- combn(unique(f), 2)
+  ret <- combi_FUN(x[i[1,],] , x[i[2,],])
+  rownames(ret) <- pasteC(i[1,], i[2,])
+  
+  # Put original colnames
+  colnames(ret) <- colnames(MAT)
+  
+  return(ret)
+}
+
+
+
+
+
+
+
+fold.change6 <- function(MAT,f,aggr_FUN=mean,combi_FUN=function(x,y) "/"(x,y)   ){
+  
+  f_un <- unique(f)
+  combs <- t(combn(as.character(f_un),2))
+  
+  out3 <- data.frame(class = f, MAT) %>%  slice_rows("class") %>% by_slice(map, aggr_FUN) %>% 
+                                          do(combi_FUN( slice(.,match(combs[,1], class))[,-1]  ,slice(.,match(combs[,2], class))[,-1]      )) %>% 
+                                          as.matrix
+  
+  # Put original dimnames
+  rownames(out3) <- pasteC(combs[,1],combs[,2])
+  colnames(out3) <- colnames(MAT)
+  
+  return(out3)
+}
 
