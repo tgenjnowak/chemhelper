3ead71ae3c14e6a09b29a2a29ea98a3f1aa48a93
Jan Stanstrup <stanstrup@gmail.com>
2016-04-18 10:51:30 +0200

    Added mzmine2xcmsSet

--- R/xcms_helpers.R
+++ R/xcms_helpers.R
@@ -145,3 +145,174 @@ annotation_statistics[10,2]=median(table(object@annoGrp[,'psgrp']))
 return(annotation_statistics)
 
 }
+
+
+
+
+
+
+mzmine2xcmsSet <- function(peaklist_mzmine, filepath = NULL, get.scantimes = FALSE){
+ 
+  # to make package check happy about non-standard evaluation.
+  type <- 
+  value <- 
+  `row ID` <- 
+  `row m/z` <- 
+  `row retention time` <- 
+  `All identity` <- 
+  egauss <- 
+  mu <-
+  sigma <-
+  h <-
+  f <-
+  dppm <-
+  scpos <-
+  scmin <-
+  scmax <-lmin <-
+  lmax <-
+  `All identity elements` <-
+  ID <-
+  `Molecular formula` <-
+  Name <-
+  `Identification method` <-
+  sample_name <-
+  `Peak m/z` <-
+  distinct <-
+  `.` <-
+  `Peak status` <-
+  `Peak RT` <-
+  `Peak RT end` <-
+  `Peak RT start` <-
+  mz <-
+  rtmax <-
+  rtmin <-
+  `Peak area` <-
+  `Peak height` <-
+  mzmin <-
+  mzmax <-
+  into <-
+  intb <-
+  maxo <-
+  sn <-
+  NULL 
+  
+  
+
+  # Check that filepath was set
+  if(is.null(filepath)){stop("filepath has to be filled and give the path to the data files files")}
+  
+  
+  
+  # Make an empty xcmsSet
+  mzmine_xset <- new("xcmsSet")
+  
+  
+  # completely made up fields
+  mzmine_xset@profinfo  <- list(method="bin",step=0.005)
+  mzmine_xset@mslevel   <- numeric(0)
+  mzmine_xset@scanrange <- numeric(0)
+  
+  
+  # Convert table to long format
+  peaklist_mzmine_long <- peaklist_mzmine %>% gather(type,value,-`row ID`,-`row m/z`,-`row retention time`,-`All identity elements`,-`ID`,-`Molecular formula`,-`Name`,-`Identification method`) %>% 
+                          separate(type,c("sample_name","type"),sep = " ",extra = "merge") %>% 
+                          mutate(type = str_replace_all(type, "filtered ", ""))
+  
+  
+  # Filter, do some fixes, and convert back to wide format
+  peaklist_mzmine_peaks <-  peaklist_mzmine_long %>% 
+                            filter(type %in% c("Peak m/z","Peak RT","Peak RT start","Peak RT end","Peak height","Peak area","Peak FWHM","Peak tailing factor","Peak asymmetry factor","Peak status")) %>% 
+                            select(`row ID`,`sample_name`,`type`,`value`) %>% 
+                            mutate(type = str_replace_all(type, "sample_name", "sample")) %>% 
+                            spread(type, value) %>% 
+                            mutate(sample = as.numeric(factor(sample_name,levels=unique(sample_name)))) %>%
+                            arrange(sample) %>% 
+                            filter(`Peak m/z`!=0)
+  
+  
+  # Get which peaks have been filled
+  mzmine_xset@filled <- which(peaklist_mzmine_peaks$`Peak status` != "DETECTED")
+  
+  
+  # take out filename so we have ordering
+  mzmine_xset@filepaths  <- peaklist_mzmine_peaks %>% 
+                            select(sample,sample_name) %>% 
+                            distinct %>% 
+                            arrange(sample) %>% 
+                            select(sample_name) %>% 
+                            as.matrix %>% as.character %>%
+                            paste(collapse="|") %>% 
+                            grep(list.files(filepath,full.names = TRUE), value=TRUE)
+  
+  
+  # Make dummy sample grouping
+  mzmine_xset@phenoData <- data.frame(class = factor(rep("dummy_class",length(mzmine_xset@filepaths))),row.names = sub("\\.[[:alnum:]]+$", "", basename(as.character(mzmine_xset@filepaths))))
+  
+  
+  # get rt of each scan for each file to fill into @rt
+  # Need to check if this can be disabled to save time if CAMERA doesn't need it
+  # we are putting the same in "raw" and "corrected"
+  if(get.scantimes){
+    
+    mzmine_xset@rt <- list(raw = vector("list",length(mzmine_xset@filepaths)),corrected = vector("list",length(mzmine_xset@filepaths)))
+    
+    for(i in 1:length(mzmine_xset@filepaths)){
+      xraw <- xcmsRaw(mzmine_xset@filepaths[i],profstep = 0)
+      mzmine_xset@rt$raw[[i]] <- mzmine_xset@rt$corrected[[i]] <- xraw@scantime
+      names(mzmine_xset@rt$raw[[i]]) <- names(mzmine_xset@rt$corrected[[i]]) <- 1:length(xraw@scantime)
+    }
+    
+  }
+  
+  
+  # Take out group indexes while we still have the grouping info
+  mzmine_xset@groupidx <- peaklist_mzmine_peaks %>% select(`row ID`) %>% 
+                          mutate(row_nr = 1:n()) %>% 
+                          as.matrix %>% 
+                          split(x = .[,"row_nr"],f = .[,"row ID"])
+  
+  names(mzmine_xset@groupidx) <- NULL
+  
+  
+  # get number of detected peaks per peak group
+  npeaks <-   peaklist_mzmine_peaks %>% 
+              select(`row ID`,`Peak status`) %>% 
+              group_by(`row ID`) %>% 
+              summarise(npeaks = sum(`Peak status`=="DETECTED")) %>% 
+              select(npeaks) %>% as.matrix %>% as.integer
+  
+  
+  # Create the final list of peaks
+  peaklist_mzmine_peaks %<>% 
+                              select(-sample_name,-`row ID`,-`Peak status`) %>%
+                              rename(mz = `Peak m/z`) %>%
+                              rename(rt = `Peak RT`, rtmax = `Peak RT end`, rtmin = `Peak RT start`) %>% 
+                              mutate_each(funs(as.numeric)) %>%
+                              mutate(mzmin = mz-0.005,mzmax = mz+0.005) %>%
+                              mutate(rt = rt*60, rtmax = rtmax*60, rtmin = rtmin*60) %>% 
+                              mutate(into = `Peak area`, intb = `Peak area`, maxo = `Peak height`) %>% 
+                              mutate(sn = NA, egauss = NA, mu = NA, sigma = NA, h = NA, f = NA, dppm = NA, scale = NA, scpos = NA, scmin = NA, scmax = NA, lmin = NA, lmax = NA) %>% 
+                              select(mz,mzmin,mzmax,rt,rtmin,rtmax,into,intb,maxo,sn,egauss,mu,sigma,h,f,dppm,scale,scpos,scmin,scmax,lmin,lmax,sample) %>% 
+                              mutate_each(funs(as.numeric)) %>% as.matrix
+  
+  mzmine_xset@peaks     <- peaklist_mzmine_peaks
+  
+  
+  # make peak group stats
+  mzmine_xset@groups <- lapply(mzmine_xset@groupidx, function(x){ data.frame( mzmed  = median(mzmine_xset@peaks[x,"mz"]),
+                                                                              mzmin  = min(   mzmine_xset@peaks[x,"mzmin"]),
+                                                                              mzmax  = max(   mzmine_xset@peaks[x,"mzmax"]),
+                                                                              rtmed  = median(mzmine_xset@peaks[x,"rt"]),
+                                                                              rtmin  = min(   mzmine_xset@peaks[x,"rtmin"]),
+                                                                              rtmax  = max(   mzmine_xset@peaks[x,"rtmax"])
+                                                                             )
+                                                                }
+                              ) %>% do.call(rbind,.) %>%
+                                    mutate(npeaks = npeaks) %>% 
+                                    mutate(dummy_class = npeaks) %>%  # I don't know what is actually supposed to go here
+                                    as.matrix
+  
+  
+  return(mzmine_xset)
+}
+
